fef0876c20ec83cc904c7c60d0f14675883728f4
Fazel Saeedi <fazels36@gmail.com>
2021-02-09 13:12:16 +0330

    implement Json Function

--- core/Response.php
+++ core/Response.php
@@ -8,4 +8,21 @@ class Response
   {
      http_response_code($code);
   }
+
+  public function Json(array $Array , $status)
+  {
+
+    // headers for not caching the results
+    header('Cache-Control: no-cache, must-revalidate');
+    header('Expires: Mon, 26 Jul 2022 05:00:00 GMT');
+
+    // headers to tell that result is JSON
+    header('Content-type: application/json');
+    http_response_code(404);
+
+    return json_encode($Array);
+
+  }
+
+
 }
