d7a13ea8eca5627f4378063b3bf9509c3c7ef23b
Fazel Saeedi <fazels36@gmail.com>
2021-02-09 13:21:34 +0330

    implement Register model and functions

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -4,23 +4,46 @@ namespace app\controllers;
 
 use app\core\Controller;
 use app\core\Request;
+use app\core\Response;
+use app\model\RegisterModel;
+use app\Repository\testRepositoryPDO;
 
 class AuthController extends Controller
 {
+  protected $repositoryPDO ;
+
+  public function __construct()
+  {
+      $this->repositoryPDO = new testRepositoryPDO();
+  }
 
   public function login(Request $request)
   {
     // return 'this is handle login' ;
 
     if ($request->isPost()) {
+
+
       return 'Handle submitted dataSsSsSsSsSs' ;
     }
 
   }
 
-  public function register()
+  public function register(Request $request)
   {
-    return 'this is handle register' ;
+
+     $registerModel = new RegisterModel();
+     $registerModel->loadData($request->getBody());
+
+
+    if(!$registerModel->validate())
+        return Response::Json($registerModel->errors , 404);
+
+
+
+    return $this->repositoryPDO->test();
+
+
   }
 
   public function test()
--- model/RegisterModel.php
+++ model/RegisterModel.php
@@ -2,7 +2,34 @@
 
 namespace app\model;
 
-class RegisterModel
+use app\core\Model;
+
+class RegisterModel extends Model
 {
 
+  public string $firstname = '';
+  public string $lastname = '';
+  public string $email = '';
+  public string $password = '';
+  public string $passwordConfirm = '';
+
+
+
+
+  public function register()
+  {
+    return true ;
+  }
+
+  public function rules(): array
+  {
+    return [
+      'firstname' => [self::RULE_REQUIRED],
+      'lastname' => [self::RULE_REQUIRED],
+      'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+      'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8] , [self::RULE_MAX, 'max' => 15]],
+      'passwordConfirm' => [[self::RULE_MATCH, 'match' => 'password']],
+    ];
+
+  }
 }
